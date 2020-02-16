be4dcc3d52032b280c52fe59bcae16d2fd42b74b
Timothy Nkata <timnkata@gmail.com>
2020-02-16 16:39:40 -0500

    Initial Commit

---
+++ FlashCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCard
+//
+//  Created by Timothy Nkata on 2/16/20.
+//  Copyright © 2020 Timothy Nkata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
