ea963f74649863e27e8b5f4bf8afe869e02dfa8a
Timothy Nkata <48990423+Tnkata@users.noreply.github.com>
2020-03-01 20:33:22 -0500

    Add files via upload

---
+++ FlashCard/CreationViewController.swift
@@ -0,0 +1,53 @@
+//
+//  CreationViewController.swift
+//  FlashCard
+//
+//  Created by Timothy Nkata on 2/29/20.
+//  Copyright © 2020 Timothy Nkata. All rights reserved.
+//
+
+import UIKit
+
+class CreationViewController: UIViewController {
+
+    var flashCardsController: ViewController!
+    
+    @IBOutlet weak var questionTextField: UITextField!
+    
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapOnCancel(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func didTapOnDone(_ sender: Any) {
+        
+        
+        let questionText = questionTextField.text
+        
+        
+        let answerText = answerTextField.text
+        
+        flashCardsController.updateFlashCard(question: questionText!, answer: answerText!)
+        
+        dismiss(animated: true) 
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
